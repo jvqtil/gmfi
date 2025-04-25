26d5445ebffc6b53352077bc4b53fc50fc058a82
Egor <109822937+jvqtil@users.noreply.github.com>
2025-04-25 17:34:01 +0300

    lets go! 1.3 is for real here!

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"flag"
 	"time"
 
 	"github.com/fatih/color"
@@ -16,22 +17,49 @@ func main() {
 	yellow := color.New(color.FgYellow).SprintFunc()
 	pink := color.New(color.FgMagenta).SprintFunc()
 	cyan := color.New(color.FgCyan).SprintFunc()
-	
+
+	helpFlag := flag.Bool("help", false, "Show help and exit")
+	hFlag := flag.Bool("h", false, "Show help and exit (shorthand)")
+	versionFlag := flag.Bool("version", false, "Show version and exit")
+	vFlag := flag.Bool("v", false, "Show version and exit (shorthand)")
+	flag.Parse()
+
+	usageHelp := "\n" + "Usage: " + green("gmfi") + blue(" <filename>")
+
+	if *helpFlag || *hFlag {
+		const helpText = `
+Options:
+	-h, --help       Show this help message
+	-v, --version    Show version information`
+		fmt.Println(usageHelp)
+		fmt.Println(helpText)
+		return
+	}
+	if *versionFlag || *vFlag {
+		const version = "1.3"
+		fmt.Println("\nVersion:", green("gmfi"), wrap(version))
+		return
+	}
+
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: " + green("gmfi") + blue(" <filename>"))
+		fmt.Println(usageHelp)
 		return
 	}
 
 	filename := os.Args[1]
 	info, err := os.Stat(filename)
 	if err != nil {
-		fmt.Println("Error:", err)
+		if os.IsNotExist(err) {
+			fmt.Println(red("\nNot found"), wrap(filename))
+		} else {
+			fmt.Println(red("\nError"), wrap(err.Error()))
+		}
 		return
 	}
 
 	absPath, err := filepath.Abs(filename)
 	if err != nil {
-		fmt.Println("Error:", err)
+		fmt.Println("\nError", wrap(err.Error()))
 		return
 	}
 
@@ -64,7 +92,7 @@ func main() {
 	fmt.Printf("%s %s\n", 
 	yellow(fmt.Sprintf("%-14s", "Is Directory? ")), 
 	wrap(toStr(info.IsDir())))
-	
+
 	fmt.Printf("%s %s\n", 
 	pink(fmt.Sprintf("%-14s", "Absolute Path ")), 
 	wrap(absPath))
